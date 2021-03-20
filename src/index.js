968fa118d5ef19a12b9c7e71228308223e739107
AndriiSorokin <andrewsorokin07@gmail.com>
2021-03-20 18:41:10 +0200

    make persist

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import store from './redux/store';
 import { Provider } from 'react-redux';
+import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
+    <Provider store={store.store}>
+      <PersistGate loading={'loading...'} persistor={store.persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,27 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware, combineReducers } from '@reduxjs/toolkit';
 import userReducer from './reducers/userReducer';
 import logger from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const middleware = [...getDefaultMiddleware(), logger];
+
+const persistConfig = {
+  key: 'contacts',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  contacts: userReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: {
-    contacts: userReducer,
-  },
+  reducer: persistedReducer,
   middleware,
   devTools: process.env.NODE_ENV === 'development',
 });
+const persistor = persistStore(store);
 
-export default store;
+export default { store, persistor };
